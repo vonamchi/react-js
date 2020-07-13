d59a69661fca74ac9c4364605385ce02c415a651
Vo Nam Chi <namchi1990@gmail.com>
2020-07-13 05:29:08 -0700

    'buoi1'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Demo from './Components/DemoComponent/Demo';
-import DemoClass from './Components/DemoComponent/DemoClass';
-import BaiTapLayout from './Components/BaiTapLayout/BaiTapLayout';
-import DataBinding from './DataBinding/DataBinding';
+import React from "react";
+import Demo from "./Components/DemoComponent/Demo";
+import DemoClass from "./Components/DemoComponent/DemoClass";
+import BaiTapLayout from "./Components/BaiTapLayout/BaiTapLayout";
+import DataBinding from "./DataBinding/DataBinding";
 
 function App() {
   return (
     <div className="App">
-      {/* <BaiTapLayout /> */}
-      <DataBinding />
+      <BaiTapLayout />
+      {/* <DataBinding /> */}
       {/* <p className="title">Hello hello</p> */}
     </div>
   );
